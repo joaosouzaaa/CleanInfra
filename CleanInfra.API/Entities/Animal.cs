8598ce64c0505ed15b1ffd0b0e24c01e4639e02f
joaosouzaaa <joaoasouza982@gmail.com>
2024-01-30 22:33:00 -0300

    Feat: Initial entities and structure

---
+++ CleanInfra.API/Entities/Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanInfra.API.Entities;
+
+public sealed class Animal
+{
+    public int Id { get; set; }
+    public required string ScientificName { get; set; }
+    public required string Species { get; set; }
+
+    public required int ZooId { get; set; }
+    public Zoo Zoo { get; set; }
+}
---
+++ CleanInfra.API/Entities/Zoo.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanInfra.API.Entities;
+
+public sealed class Zoo
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public decimal EntryPrice { get; set; }
+
+    public List<Animal> Animals { get; set; }
+}
